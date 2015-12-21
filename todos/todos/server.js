dff7d1227af20967a898d353c5aa20e3cedec453
alexandrkopylov <5186441@gmail.com>
2015-12-21 20:55:14 +0300

    add cookie Parser

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -19,12 +19,16 @@ var Todo = mongoose.model('Todo', {
     text: String
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ 'extended': 'true' }));         // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({ 'extended': 'true' }));         // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+    app.use(express.cookieParser());
+})
+
 
 app.get('/api/todos', function (req, res) {
 
