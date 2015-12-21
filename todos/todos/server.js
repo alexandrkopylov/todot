ccaf8e0f02fddef5e82121d77b4c06ca3897ad50
alexandrkopylov <5186441@gmail.com>
2015-12-21 18:12:56 +0300

    added json and methodovverride

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -7,6 +7,10 @@ mongoose.connect('mongodb://localhost/todo');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));         // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
 app.get('/', function (req, res) {
     console.log('Get /');
