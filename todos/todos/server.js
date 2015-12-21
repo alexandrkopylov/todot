ffa46d7cf2a75a650ca38ad66746a9ad079cf0b4
alexandrkopylov <5186441@gmail.com>
2015-12-21 09:35:05 +0300

    changes re for get 1

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -2,7 +2,8 @@
 var app = express();
 
 app.get('/', function (req, res) {
-  console.log('Get /')
+    console.log('Get /');
+    res.display('sss');
 });
 app.listen(8080)
 console.log("App listening on port 8080");
