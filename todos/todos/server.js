a78d6c02c052ceb05f42757b96d83ed60d58c3bd
alexandrkopylov <5186441@gmail.com>
2015-12-21 09:32:25 +0300

    add get /

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -1,5 +1,8 @@
 ﻿var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  console.log('Get /')
+});
 app.listen(8080)
-console.log("App listening on port 80");
+console.log("App listening on port 8080");
