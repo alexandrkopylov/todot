48833c03d40f5e5f6ca1bde939f0e7b5d06eb239
alexandrkopylov <5186441@gmail.com>
2015-12-21 19:01:24 +0300

    post delete get todos added

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -39,6 +39,43 @@ app.get('/api/todos', function (req, res) {
     });
 });
 
+app.post('/api/todos', function (req, res) {
+
+    // create a todo, information comes from AJAX request from Angular
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function (err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function (err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function (req, res) {
+    Todo.remove({
+        _id: req.params.todo_id
+    }, function (err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function (err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
 // define tables
 
 
