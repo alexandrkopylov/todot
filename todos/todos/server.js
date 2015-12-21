79e773219dc2a78f1436f07681bbe09f4fe62f77
alexandrkopylov <5186441@gmail.com>
2015-12-21 17:04:57 +0300

    add get todo

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -6,6 +6,11 @@ app.get('/', function (req, res) {
     res.send('sss');
 });
 
+app.get('/todo', function (req, res) {
+    console.log('Get todo');
+    res.send('get todo');
+});
+
 var server = app.listen(8080, function () {
     console.log("App listening on port 8080");
 });
