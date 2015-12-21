22ca2ef970e30318fb960bbf8e90fe99e3c2da84
alexandrkopylov <5186441@gmail.com>
2015-12-21 19:06:37 +0300

    add index.html

--- todos/todos/server.js
+++ todos/todos/server.js
@@ -76,9 +76,9 @@ app.delete('/api/todos/:todo_id', function (req, res) {
     });
 });
 
-// define tables
-
-
+app.get('*', function (req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 
 // starting server
