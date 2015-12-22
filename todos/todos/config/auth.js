d03e31dc1ef9f7ef1260c84709d394c65466a800
alexandrkopylov <5186441@gmail.com>
2015-12-22 11:41:14 +0300

    correction

--- todos/todos/config/auth.js
+++ todos/todos/config/auth.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
     facebookAuth:{
-        'clientUD' :'156257574737264',
+        'clientID' :'156257574737264',
         'clientSecret' : 'e007cae4c13e014919d5b3eb7f82265c',
         'claabackURL': 'http://104.154.78.97:8080/auth/facebook/callback'
     }
