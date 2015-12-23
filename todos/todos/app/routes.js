7b29e002509c1fcb0ac9855b7a117f2f799945e0
alexandrkopylov <5186441@gmail.com>
2015-12-23 10:55:08 +0300

    ej

--- todos/todos/app/routes.js
+++ todos/todos/app/routes.js
@@ -66,7 +66,7 @@ module.exports = function(app,passport,Todo){
         console.log('Sending indexpage');
         res.json({"userno":"20"})
     });
-    app.get('/', function (req,res){
+    app.get('/ej', function (req,res){
         res.render('index.ejs');
     })
 
