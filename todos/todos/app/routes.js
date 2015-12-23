b875626671b5e9e0501c03b9e08eb8c57327ad17
alexandrkopylov <5186441@gmail.com>
2015-12-23 09:34:18 +0300

    tcr

--- todos/todos/app/routes.js
+++ todos/todos/app/routes.js
@@ -62,8 +62,7 @@ module.exports = function(app,passport,Todo){
       })
     );
     app.get('*', function (req, res) {
-        res.json(req.user);
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
         console.log('Sending indexpage');
     });
 
